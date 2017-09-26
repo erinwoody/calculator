111616f6aa6782f2108f2ba03531df85c5f65dcd
Erin Woody <erinelizabethwoody@gmail.com>
2017-09-26 15:04:06 -0500

    buttons working

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-var
-var
-var
-var
-
-function ()
-
-function ()
-
-function ()
+function Clear() {
+  document.calculator.display.value = "";
+}
+function Divide() {
+  document.calculator.display.value += "/";
+}
+function Multiply() {
+  document.calculator.display.value += "*";
+}
+function Subtract() {
+  document.calculator.display.value += "-";
+}
+function Add() {
+  document.calculator.display.value += "+";
+}
+function Percent() {
+  document.calculator.display.value += "%";
+}
+function OpenParen() {
+  document.calculator.display.value += "(";
+}
+function CloseParen() {
+  document.calculator.display.value += ")";
+}
